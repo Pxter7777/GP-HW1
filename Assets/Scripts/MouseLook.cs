499bbd5d9dff40c356627090bc2b3bd7141a1980
Pxter7777 <pxter.cs05@nctu.edu.tw>
2023-10-09 20:15:07 +0800

    add mouse control look

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float sensitivity = 2.0f;
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotationX -= mouseY;
+        rotationY += mouseX;
+
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+        
+        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
+    }
+}
