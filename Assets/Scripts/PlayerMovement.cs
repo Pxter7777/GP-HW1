9892c40c8f5b18846a691f2664ac373a78ea7766
Pxter7777 <pxter.cs05@nctu.edu.tw>
2023-10-09 20:50:52 +0800

    Player can move by using WASD

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        // Get input.
+        float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right
+        float vertical = Input.GetAxis("Vertical"); // W/S or Up/Down
+
+        // Calculate movement direction based on player's forward and right vectors.
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = (forward * vertical) + (right * horizontal);
+        if (moveDirection.sqrMagnitude > 1)
+        {
+            moveDirection.Normalize();
+        } 
+        // Apply movement.
+        transform.position += moveDirection * speed * Time.deltaTime;
+    }
+}
